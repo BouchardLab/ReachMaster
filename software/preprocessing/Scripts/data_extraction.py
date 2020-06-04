81df01ce9bf966b8cc1c1561f3cd306186953ecc
Brett Nelson <brrnelson@berkeley.edu>
2020-06-03 19:13:52 -0700

    Commit before work
    Signed-off-by: Brett Nelson <brrnelson@berkeley.edu>

--- software/preprocessing/Scripts/data_extraction.py
+++ software/preprocessing/Scripts/data_extraction.py
@@ -26,7 +26,7 @@ def load_files(analysis=False):
         time = trodes_data['time']
         true_time = match_times(controller_data, trodes_data)
         reach_indices = get_reach_indices(controller_data)
-        successful_trials = get_successful_trials(controller_data, true_time)
+        successful_trials = get_successful_trials(controller_data, true_time, trodes_data)
         exposures = get_exposure_times(trodes_data['DIO']['top_cam'])
         reach_masks = get_reach_times(true_time, reach_indices)
 
