651467efae24a1990d97eb5a5e48960ab2eee798
Brett Nelson <brrnelson@berkeley.edu>
2020-06-03 19:23:58 -0700

    Working extraction from trodes/calibration
    Signed-off-by: Brett Nelson <brrnelson@berkeley.edu>

--- software/preprocessing/Scripts/data_extraction.py
+++ software/preprocessing/Scripts/data_extraction.py
@@ -20,7 +20,6 @@ def load_files(analysis=False):
     trodes_data = import_trodes_data(trodes_dir, exp_name, win_dir=True)
     config_data = import_config_data(config_dir)
     controller_data = import_controller_data(controller_path)
-    print(trodes_data)
     # analysis
     if analysis:
         time = trodes_data['time']
@@ -29,9 +28,13 @@ def load_files(analysis=False):
         successful_trials = get_successful_trials(controller_data, true_time, trodes_data)
         exposures = get_exposure_times(trodes_data['DIO']['top_cam'])
         reach_masks = get_reach_times(true_time, reach_indices)
+        r_m = pd.Series({'reach_masks': reach_masks})
+        e = pd.Series({'exposures': exposures})
+        r_i = pd.Series({'ri': reach_indices})
+        s_t = pd.Series({'s_t': successful_trials})
+        r_m.to_csv('r_m.csv')
+        e.to_csv('e.csv')
+        r_i.to_csv('r_i.csv')
+        s_t.to_csv('s_t.csv')
 
-        edf = pd.DataFrame({'successful_trials': successful_trials, 'exposures': exposures, 'reach_masks': reach_masks,
-                            'controller_data': controller_data, 'trodes_time': time, 'true_time': true_time})
-        edf.to_csv(save_path)
-
-    return controller_data, trodes_data, config_data
+    return successful_trials
