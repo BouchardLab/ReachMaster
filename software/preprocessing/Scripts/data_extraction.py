a50b8d6f964a09d343d63f1fdd0fddc84b3cd8c6
Brett Nelson <brrnelson@berkeley.edu>
2020-06-24 22:21:08 -0700

    Updated
    Signed-off-by: Brett Nelson <brrnelson@berkeley.edu>

--- software/preprocessing/Scripts/data_extraction.py
+++ software/preprocessing/Scripts/data_extraction.py
@@ -1,18 +1,20 @@
 """Script to import trodes, micro-controller, and config data
 
 """
-import numpy as np
 import glob
 import os
+
+import numpy as np
+import pandas as pd
+
 from software.preprocessing.config_data.config_parser import import_config_data
 from software.preprocessing.controller_data.controller_data_parser import import_controller_data, get_reach_indices, \
     get_reach_times
-from software.preprocessing.reaching_without_borders.rwb import match_times, get_successful_trials, make_trial_masks
+from software.preprocessing.reaching_without_borders.rwb import match_times, get_successful_trials
 from software.preprocessing.trodes_data.experiment_data_parser import import_trodes_data, get_exposure_times
 
 
-
-def load_files(trodes_dir,exp_name,controller_path,config_dir,save_path, analysis=False,scrape=False):
+def load_files(trodes_dir, exp_name, controller_path, config_dir, save_path, analysis=False, scrape=False):
     # importing data
     trodes_data = import_trodes_data(trodes_dir, exp_name, win_dir=True)
     config_data = import_config_data(config_dir)
@@ -29,10 +31,9 @@ def load_files(trodes_dir,exp_name,controller_path,config_dir,save_path, analysi
         reach_masks = get_reach_times(true_time, reach_indices)
         reach_masks_start = np.asarray(reach_masks['start'])
         reach_masks_stop = np.asarray(reach_masks['stop'])
-        trial_masks = make_trial_masks(controller_data, trodes_data)
         reach_indices_start = reach_indices['start']
         reach_indices_stop = reach_indices['stop']
-
+        trial_masks = trial_mask(true_time, reach_indices_start, reach_indices_stop)
         np.savetxt('reach_masks_start.csv', reach_masks_start, delimiter=',')
         np.savetxt('reach_masks_stop.csv', reach_masks_stop, delimiter=',')
         np.savetxt('succ_trials.csv', np.asarray(successful_trials), delimiter=',')
@@ -75,19 +76,48 @@ def name_scrape(file,pns):
     controller_path = path_deleveled + 'sensor_data/'
     return controller_path, config_path, exp_name
 
+
 def host_off(save_path):
     Cns = '~/bnelson/CNS/'
     pns = '~/bnelson/PNS_data/'
     # cns is laid out rat/day/session/file_name/localdir (we want to be in localdir)
     # search for all directory paths containing .rec files
     os.chdir(Cns)
+    i = 0
     for file in glob.glob('*.rec*'):
-        controller_path,config_path,exp_name = name_scrape(file,pns)
-        df = load_files(file, exp_name, controller_path, config_path, save_path, analysis=True)
-
-    return df
+        controller_path, config_path, exp_name = name_scrape(file, pns)
+        print(exp_name + ' is being added..')
+        list_of_df = load_files(file, exp_name, controller_path, config_path, save_path, analysis=True)
+        if i == 0:
+            main_df = list_of_df
+        else:
+            main_df = pd.concat([main_df, list_of_df], axis=1)
+        i += 1
+    print('Finished!!')
+    return main_df
 
 
 def to_df():
-    df=''
-    return df
+    c = pd.MultiIndex.from_product([[scorer], [rat], [date], [session], [dim], [i], [part], [trial],
+                                    [total], ['x', 'y', 'z']],
+                                   names=['Scorer', 'Rat', 'Date', 'Session', 'trial_dim', 'trials', 'Bodypart', 'S/F',
+                                          'exp_times', 'coords'])
+    new_df = pd.DataFrame(new_trial, columns=c)
+    new_df.keys
+    if i == 0:
+        trial_dataframe = new_df
+    else:
+        trial_dataframe = pd.concat([trial_dataframe, new_df], axis=1)
+    return trial_dataframe
+
+
+def trial_mask(matched_times, r_i_start, r_i_stop, s_t):
+    new_times = np.zeros(len(matched_times[0]))
+    for i, j in zip(range(0, len(r_i_start) - 1), range(0, len(r_i_stop) - 1)):
+        ix = int(r_i_start[i])
+        jx = int(r_i_stop[i])
+        if any(i == s_t):
+            new_times[ix:jx] = 2
+        else:
+            new_times[ix:jx] = 1
+    return new_times
