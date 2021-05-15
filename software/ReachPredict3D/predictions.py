d3385b62263bab7c0246c0f75503dbcc2df81f48
Brett Nelson <43356326+throneofshadow@users.noreply.github.com>
2021-05-15 00:10:32 -0700

    Rewrote our lab's DeepLabCut prediction (predictions.py) and 3-D reconstruction (reconstruct_predictions.py). Took out lots of old functions, rewrote back-end code inside of data_extraction.py as well as removed many non-essential functions

---
+++ software/ReachPredict3D/predictions.py
@@ -0,0 +1,96 @@
+"""This module is intended to provide methods to iterate over all video directories
+for a given rat, split the videos into individual camera videos if this has not
+yet been done, and use a pre-trained DLC network to obtain 2-D predictions of
+experimental data for multiple rat positions.
+"""
+
+import os
+import deeplabcut
+import tensorflow as tf
+import glob
+
+
+def find_cam_files(root_dir):
+    """
+    Function to find cam files for DeepLabCut to run our predictive network on!
+
+    Attributes
+    ----------
+    root_dir : path directory
+    Returns
+    -------
+    cam1_array: list of paths to experimental video files from camera1
+    cam2_array: list of paths to experimental video files from camera2
+    cam3_array: list of paths to experimental video files from camera3
+    """
+    cam1_array = []
+    cam2_array = []
+    cam3_array = []
+    for file in glob.glob(root_dir, recursive=True):
+        path = file.rsplit('/', 1)[0]
+        path = path+'/'
+        if "shuffle2" in file:
+            print("File has been analyzed already!" + file)
+            sig_flag = 0
+        else:
+            sig_flag = 1
+        if "cam1" in file: #check and make sure that the files have been split
+            if sig_flag == 1:
+                cam1_array.append(file)
+        elif "cam2" in file:
+            if sig_flag == 1:
+                cam2_array.append(file)
+        elif "cam3" in file:
+            if sig_flag == 1:
+                cam3_array.append(file)
+    return cam1_array, cam2_array, cam3_array
+
+
+def run_analysis_videos(cam_video_paths, config_path, filtering=False):
+    """ function to run deeplabcut on a list of videos from a single cam
+    Attributes
+    -------------
+    cam_video_paths: list of video paths from individual cams
+    config_path: DLC config path
+    filtering: boolean, uses (at the moment) median filtering from DLC
+
+    """
+    shuffle = 2 # shuffle of the network to use
+    print("Starting to extract files..")
+    deeplabcut.analyze_videos(config_path, cam_video_paths, videotype='mp4', shuffle=shuffle, save_as_csv=True)
+    if filtering:
+        deeplabcut.filterpredictions(config_path, cam_video_paths, videotype='.mp4', shuffle=shuffle)
+
+
+def run_main(root_dir, config, fset=False):
+    """ function intended to loop over PNS directory, obtain each camera's files, and send them to DLC.
+    Attributes
+    ------------
+    root_dir: path to PNS root_dir to iterate over
+    config: path to DLC config file
+    fset: boolean, filtering flag
+    """
+    pathList = []
+    cam1_array, cam2_array, cam3_array = find_cam_files(root_dir, config)
+    #print(cam1_array)
+    print('Starting Cam 1 DLC analysis')
+    #sleep(5)
+    print('Number of Videos to analyze :  ' + str(len(cam1_array)))
+    run_analysis_videos(cam1_array,config, filtering=fset)
+    print('Starting Cam 2 DLC analysis')
+    print('Number of Videos to analyze :  ' + str(len(cam2_array)))
+    run_analysis_videos(cam2_array,config,filtering=fset)
+    print('Starting Cam 3 DLC analysis')
+    print('Number of Videos to analyze :  ' + str(len(cam3_array)))
+    run_analysis_videos(cam3_array,config,filtering=fset)
+    print('Finished extracting 2-D DLC estimates!')
+
+
+class GetPredictions:
+    """ Class to run DLC over a given Rat's PNS directory containing experimental video. Resulting
+    files are saved inside of the PNS folder, filtering is optional!"""
+    def __init__(self,root_dir,config_dir, GPU_num=0):
+        os.environ["CUDA_VISIBLE_DEVICES"] = GPU_num
+        gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.75)
+        sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options))
+        run_main(root_dir, config_dir)
---
+++ software/ReachPredict3D/reconstruct_predictions.py
@@ -0,0 +1,97 @@
+"""
+
+"""
+
+from predictions import find_cam_files
+import os
+import glob
+import tqdm
+import pickle
+from software.preprocessing.Scripts.data_extraction import name_scrape, get_name
+import pandas as pd
+from software.preprocessing.video_data.DLC.Reconstruction import get_kinematic_data
+
+
+def return_block_kinematic_df(f):
+    """function to extract a single experimental session of positional predictions in 3-D using DLT
+    Attributes
+    --------------
+    f: list containing file path and dlt_path used to generate PNS directory for kinematic data scrape
+
+    Returns
+    -----------
+    kd : 3-D predictions for a given session, pandas dataframe
+    """
+    file_ = f[0]
+    dlt_path = f[1]
+    controller_path, config_path, exp_name, name, ix, trodes_name, video_path = name_scrape(file_)
+    # dim, reward_dur, x_pos, y_pos, z_pos, x0, y0, z0, r, t1, t2 = get_config_data(str(config_path))
+    date = get_name(file_)
+    print(exp_name + ' is being added..')
+    kd = get_kinematic_data(video_path, dlt_path, 'resnet', name, date, ix, 0)
+    return kd
+
+
+def save_kinematics(unpickled_list):
+    """ function intended to save list of dataframes from a given rat as a single dataframe
+    Attributes
+    ------------
+    unpickled_list: list containing all N dataframes from a given rat's session
+
+    Returns
+    -------------
+    df1: dataframe containing all N dataframes for a given rat"""
+    encountered_df = False
+    for i in range(len(unpickled_list)):
+        rat_df1 = unpickled_list[i]
+        if ((rat_df1 is not 0) and (type(rat_df1) is not list)):
+            if (not encountered_df):
+                encountered_df = True
+            # create a new dataframe (copy of original), then removes levels corresponding to (rat, date, session, dim)
+            df1 = rat_df1.droplevel([0, 1, 2, 3], axis=1)
+            # inserts columns and data for (rat, date, session, dim)
+            pos_arr = [0, 1, 1, 3]  # order to insert columns (rat, date, session, dim)
+            for i in range(4):
+                col_name = rat_df1.columns.names[i]
+                val = rat_df1.columns.levels[i][0]
+                df1.insert(pos_arr[i], col_name, val)
+            else:
+                df2 = rat_df1.droplevel([0, 1, 2, 3], axis=1)
+                pos_arr = [0, 1, 1, 3]  # order to insert columns (rat, date, session, dim)
+                for i in range(4):
+                    col_name = rat_df1.columns.names[i]
+                    val = rat_df1.columns.levels[i][0]
+                    df2.insert(pos_arr[i], col_name, val)
+                df1 = pd.concat([df1, df2], axis=0, sort=False)  # concat new df to existing df
+    df1 = df1.set_index(['rat', 'date', 'session', 'dim'])
+    return df1
+
+
+class Reconstruct3D:
+    """ Class to iterate over PNS directory sessions and return/save 3-D positional predictions from individual session files. At the
+    end of iterating over a PNS directory, the entire dataframe is saved.
+    """
+    def __init__(self):
+        self.pickling = False
+        self.rat_name = '16'
+        self.cns_path = '/clusterfs/bebb/users/bnelson/CNS/' + self.rat_name
+        self.cns_pattern = self.cns_path + '/**/*.rec'
+        self.pns_path = '/clusterfs/bebb/users/bnelson/PNS_data/'
+        self.cns = '/clusterfs/bebb/users/bnelson/CNS'
+        self.dlt_path = ''
+        self.save_path='new_file.csv'
+        self.cl = glob.glob(self.cns_pattern, recursive=True)
+        print(str(len(self.cl)) + ' of experimental blocks for this rat.')
+        d = []
+        for file in tqdm(glob.glob(self.cns_pattern, recursive=True)):
+            dc = return_block_kinematic_df([file, self.dlt_path])
+            d.append(dc)
+        print('Saving DataFrame')
+        os.chdir(self.cns)
+        if self.pickling:
+            with open(self.save_path, 'wb') as output:
+                pickle.dump(d, output, pickle.HIGHEST_PROTOCOL)
+        self.new_df = save_kinematics(d)
+        self.new_df.to_hdf(self.new_df, self.save_path) # saves total list
+
+
